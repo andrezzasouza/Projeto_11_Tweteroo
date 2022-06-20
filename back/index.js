1ee2ec9e8206e1cf7bcbb15e78ddc17777b5cc5c
Victor Hugo Meurer <meurervictorhugo@gmail.com>
2022-06-19 23:22:03 -0300

    Feat: add sign-up route

--- back/index.js
+++ back/index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(json());
+app.use(cors());
+const users = [];
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+    users.push(req.body);
+    res.send("OK");
+});
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000")
