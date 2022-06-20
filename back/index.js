f1e5e7db72c2751fd7943bd3a9242d788944ccfd
Victor Hugo Meurer <meurervictorhugo@gmail.com>
2022-06-20 13:15:25 -0300

    Minor fix

--- back/index.js
+++ back/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(json());
+app.use(express.json());
 app.use(cors());
 const users = [];
 const tweets = [];
@@ -26,6 +26,4 @@ app.get("/tweets", (_req, res) => {
     res.send(latestTweets);
 });
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000")
-});
+app.listen(5000);
