3055ce12e3a2d883087eba64db30fdbcd116198d
Victor Hugo Meurer <meurervictorhugo@gmail.com>
2022-06-20 00:15:30 -0300

    Feat: add get tweets route

--- back/index.js
+++ back/index.js
@@ -17,6 +17,15 @@ app.post("/tweets", (req, res) => {
     res.send("OK");
 });
 
+app.get("/tweets", (_req, res) => {
+    const latestTweets = tweets.slice(-10);
+    latestTweets.reverse();
+    for(let i of latestTweets) {
+        i.avatar = user.find(user => user.username === i.username).avatar;
+    }
+    res.send(latestTweets);
+});
+
 app.listen(5000, () => {
     console.log("Server is running on port 5000")
 });
